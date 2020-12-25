6bfa2dfa7cd235844d78b5d6ca97ea5ecda944b2
Henri Renzelmann <henrirpunkt@web.de>
2020-12-25 16:35:53 +0100

    minor changes

--- dataorganization.py
+++ dataorganization.py
@@ -52,7 +52,7 @@ def dataconverter(datarange):
             depth = np.transpose(currentdata)[0]
             uveloc = np.transpose(currentdata)[1]
             vveloc = np.transpose(currentdata)[2]
-            #depths.append(depth)
+            depths.append(depth)
             np.append(vvelocs, np.array(vveloc), axis=0)
             np.append(uvelocs, uveloc, axis=0)
             lats.append(lat.split(" "))
@@ -61,7 +61,7 @@ def dataconverter(datarange):
             if len(vveloc) > maxveloc:
                 maxveloc = len(vveloc)
 
-    print(np.array(vveloc))
+    print("vveloc: ", np.array(vveloc))
 
     for i in range(0, len(datarange)):
         lats[i][0] = lats[i][0].strip("°N")
@@ -76,7 +76,6 @@ def dataconverter(datarange):
 #        if len(vvelocs[i]) < maxveloc:
 #            difference = maxveloc - len(vvelocs[i])
 
-
     for i in range(0, len(datarange)-1):
         distance.append(np.sqrt(((deccords[i+1][0] - deccords[i][0]) ** 2 + (deccords[i+1][1] - deccords[i][1]) ** 2)))
         print(distance[i])
@@ -85,8 +84,28 @@ def dataconverter(datarange):
     return depths, uvelocs, vvelocs, distance
 
 
-ds, us, vs, dcs = dataconverter((31, 32))
-#print("depths:", ds)
-#print("uvelocs:", us)
-#print("uvelocs2:", len(vs[1]))
-#print("distance:", dcs)
+def interpolator(mat, distance):
+    """
+        Interpoltes the matrix mat and returns the matrix with steady steps.
+    """
+
+    gesdistance = np.zeros(len(distance))
+    gesdistance[0] = distance[0]
+    gesdis = distance[0]
+    for i in range(1, len(distance)):
+        gesdistance[i] = gesdistance[i-1] + distance[i]
+        gesdis = gesdis + distance[i]
+
+    for wth in range(1, mat.size/len(mat)-1):
+        for leng in range(len(mat)):
+            xmed = wth*gesdis/len(mat)
+            if gesdistance[wth-1] < wth*xmed:
+                newmat[wth, leng] = (1-(wth*xmed - gesdistance[wth])/(wth*xmed-gesdistance[wth]+wth*xmed-gesdistance[wth+1]))
+
+
+ds, us, vs, dcs = dataconverter((31, 32, 33, 34, 35, 38))
+print("depths:", ds)
+#print("len(depths): ", w(ds))
+print("uvelocs:", us)
+print("uvelocs2:", vs)
+print("distance:", dcs)
